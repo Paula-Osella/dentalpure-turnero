0363c77f6a30a30a84b76b69810a86ab29bba174
Paula-Osella <paulaosella19@gmail.com>
2026-03-20 21:58:34 -0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import dotenv from "dotenv"
 const app = express()
 const PORT = process.env.PORT || 3000
 dotenv.config()
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json())
 
 app.use("/api", appointmentRoutes)
